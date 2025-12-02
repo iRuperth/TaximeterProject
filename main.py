6f3791ba078b70ae320968b935a48e07c205126c
iRuperth <ruperthlosada@gmail.com>
2025-12-02 13:16:53 +0100

    feat:Adding movement fare #4

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def welcome():
-    clear_console()
+    # clear_console()
     print("Welcome to our digital Taxi")
     print("Could you please enter your name")
     name = input().strip()
-    print(f"Hello, {name}!")
-    print(f"The base rate is {base_rate:.2f} €.")
-    print(f"The stopped rate is {stopped_rate:.2f}€ per second.")
-    print(f"The moving rate is {moving_rate:.2f}€ per second.")
+    print(f"Helloo, {name}!")
+    print(f"The minimun fare is {base_rate:.2f} €.")
+    print(f"The stopped fare is {stopped_rate:.2f}€ per second.")
+    print(f"The moving fare is {moving_rate:.2f}€ per second.")
     print("Do you wish to continue? (y/n)")
     answer = input().strip().lower()
     if answer == "y":
@@ -34,41 +34,57 @@ def main():
         starting_time_journey = time.time()
         starting_time_state = starting_time_journey
         # current_state = "Moving" Starting state.
-        print("\nBe welcome and enjoy your journey! Current state: Moving")
+        print("\nBe welcome and enjoy your journey! Current state: moving")
         print(f"Current fare: {total_price:.2f} €.")
-        print("Instructions: Use 'X' for exit.")
-        
+        print("Instructions: Use 'S' for stop, 'M' for move, and 'X' for exit.")
         #Starting infinite loop for the journey.
-        while True:
-            answer = input("> ").strip().lower()
+        current_state = "moving"
 
+
+        while True:
+            answer = input("\n> ").strip().lower()
             #getting the current time.
             current_time = time.time()
             #getting the duration of the journey.
             duration = current_time - starting_time_state
+
             #adding the duration to the total price.
-            total_price += moving_rate * duration
+            if current_state == "moving":
+                total_price += moving_rate * duration
+            else:
+                total_price += stopped_rate * duration
+
             #resetting the starting time state.
             starting_time_state = current_time 
 
-            if answer == "x":
-                print("-> Ending journey...")
-                break
-            else:
-                print("Invalid command. Use X to exit.")
+            if answer == "s":
+                print("Traffic lights, good heavens!!")
+                current_state = "stopped"
 
-            print(f"-> Accumulated fare so far: {total_price:.2f} €")
+            elif answer == "m":
+                print("There we go!")
+                current_state = "moving"
+
+            # elif answer == "x":
+            #     print("Lets park...")
+            #     break
+
+            else:
+                print("Ouch, Technical error, try to press the right key. (S to stop, M to move, X to exit)")
+
+            # print(f"-> Accumulated fare so far: {total_price:.2f} €")
 
         end_time_journey = time.time()
         total_duration = end_time_journey - starting_time_journey
         
         print("\n----------------------------------------")
-        print("            JOURNEY SUMMARY              ")
+        print("|            JOURNEY SUMMARY           |")
+        print("|--------------------------------------|")
+        print(f"| Total duration: {total_duration:.2f} seconds.        |")
+        print(f"| Total price: {total_price:.2f} €.                |")
+        print("| Thank you for using our digital taxi.|")
         print("----------------------------------------")
-        print(f"Total duration: {total_duration:.2f} seconds")
-        print(f"Total price: {total_price:.2f} €")
-        print("Thank you for using our digital taxi.")
-        print("----------------------------------------")
+
 
 if __name__ == "__main__":
     main()
