ae012f4114f568d7c14cf4227ac8097ed526ea45
iRuperth <ruperthlosada@gmail.com>
2025-12-04 18:40:22 +0100

    fix: fixing conflics with historical and testing branch #13

--- test.py
+++ test.py
@@ -1,43 +1,37 @@
-# #test.py 
-# import unittest
-# from main import welcome
+import unittest
+from unittest.mock import patch, MagicMock, call
+import io
+from main import welcome, main 
+
+
+class TestMain(unittest.TestCase):
+
+    #mocking sys.stdout to capture console output.
+    @patch('sys.stdout', new_callable=io.StringIO)
+    #mocking 'input' to inject automatic responses.
+    @patch('builtins.input')
+    #mocking time.sleep to avoid real time delay.
+    @patch('time.sleep', return_value=None)
+    #return value "none" because we don't want to wait for real time delay.
+    def test_main(self, mock_sleep, mock_input, mock_stdout):
+        
+        # When called Maple, y, s, m, x.
+        mock_input.side_effect = ['Maple', 'y', 's', 'm', 'x']
+        
+        # Executing the main function.
+        main()
+        
+        # Verification of results, Capturing all console output during main execution.
+        captured_output = mock_stdout.getvalue().strip()
+        
+        # Verifying that the specific text is present in the total output.
+        expected_phrase = "Printer sound."
+        
+        self.assertIn(expected_phrase, captured_output)
+if __name__ == "__main__":
+    unittest.main()
 
 
 
-# class TestWelcome(unittest.TestCase):
-#     def main(self):
-#         print(" Printer sound.") # Prints to console
-#         expected = " Printer sound."
-#         assert print(" Printer sound.") == expected # This line fails
-
-
-
-#         # welcome() # Prints " Welcome to our digital Taxi"
-#         # expected = " Welcome to our digital Taxi"
-#         # assert welcome() == expected # This line also fails
-
-#     # current_state = "moving"
-#     # expected_state = "moving"
-#     # assert current_state == expected_state
-    
-#     # current_state = "stopped"
-#     # expected_state = "stopped"
-#     # assert current_state == expected_state
-
-#     # current_state = "parked"
-#     # expected_state = "parked"
-#     # assert current_state == expected_state  
-
-# # class TestAddFunction(unittest.TestCase):
-# #     def test_main(self):
-# #         self.assertEqual(main(), True)
-
-#     # def test_add_negative_numbers(self):
-#     #     self.assertEqual(add(-1, -2), -3)
-
-#     # def test_add_mixed_numbers(self):
-#     #     self.assertEqual(add(1, -2), -1)
-#     #     self.assertEqual(add(-1, 2), 1)
-
-# if __name__ == '__main__':
-#     unittest.main()
+# with open('test.txt', 'w') as file:
+#     file.write('testing!')
